20888d64c1c7bd087b5a2ef78435a6aeb600f9e3
Harish S <harish@harish.local>
2019-03-07 12:47:26 +0530

    show directions between fixed locations (meghana foods and everest engineering)

--- App.js
+++ App.js
@@ -7,24 +7,41 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MapView from 'react-native-maps'
-
+import MapViewDirections from 'react-native-maps-directions'
+import Config from 'react-native-config'
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor() {
+        super();
+        this.state = {
+            origin: { longitude: 77.609236, latitude: 12.973271 },
+            destination: { longitude: 77.637401, latitude: 12.970359 },
+            initialRegion: {
+                longitude: 77.6198905,
+                latitude: 12.9703533,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }
+        }
+    }
     render() {
+
         return (
             <MapView
-                style={{flex: 1}}
-                region={{
-                    latitude: 42.882004,
-                    longitude: 74.582748,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}
-                showsUserLocation={true}
-            />
+                style={{ flex: 1 }}
+                region={this.state.initialRegion}
+            >
+                <MapViewDirections
+                    origin={this.state.origin}
+                    destination={this.state.destination}
+                    apikey={Config.GOOGLE_MAPS_API_KEY}
+                    strokeColor="blue"
+                    strokeWidth={3}
+                />
+            </MapView>
         );
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+Create a file called `.env` to put secrets only in local. It is gitignored.
+Contents are:
+```
+GOOGLE_MAPS_API_KEY=xxxxxxx
+```
