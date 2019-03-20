4e18e4414f110fc9245646492bbffd9e48bd96d3
Harish S <harish@everest.engineering>
2019-03-20 10:38:33 +0530

    Adjust fontsize as per device width

--- app/Components/Statistics.js
+++ app/Components/Statistics.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text,Dimensions } from "react-native";
 
 export default class Statitics extends Component {
     render() {
@@ -10,31 +10,31 @@ export default class Statitics extends Component {
                     <Text style={styles.distance}>{this.props.distance} Km</Text>
                 </View>
                 <View>
-                    <Text style={styles.reachedText}>{this.props.reachedText}</Text>
+                    <Text numberOfLines={1} style={styles.reachedText}>{this.props.reachedText}</Text>
                 </View>
             </View>
         );
     }
 }
+const width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
     stats: {
         flexDirection: "row",
-        padding: 10,
-        bottom: 10
+        padding: 10
     },
     distance: {
         fontWeight: "400",
-        fontSize: 16,
+        fontSize: width*0.05,
         left: 11
     },
     reachedText: {
-        fontSize: 28,
+        fontSize: width*0.06,
         left: 25,
         top: 10
     },
     duration: {
         left: 10,
         fontWeight: "800",
-        fontSize: 24
+        fontSize: width*0.06,
     }
 });
